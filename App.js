5657303e5733bb9aa8345f7e7d8d32cfbf9b5826
Ian Njoroge <iannjoroge120@gmail.com>
2022-07-19 00:08:55 +0300

    intializing

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import HomeScreen from "./screens/HomeScreen";
+import DriverScreen from "./screens/DriverScreen";
+import DriverProfile from "./screens/DriverProfile";
+
+import { Colors } from "./constants/styles";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
+import { useContext } from "react";
+import IconButton from "./components/ui/IconButton";
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
+
+  function logout() {
+    authCtx.logout();
+  }
+  function profile() {
+    console.log("Driver Profile");
+  }
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+      }}
+    >
+      <Stack.Screen
+        name="Driver"
+        component={DriverScreen}
+        options={({ navigation }) => ({
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              size={24}
+              onPress={logout}
+            />
+          ),
+          headerLeft: ({ tintColor }) => (
+            <IconButton
+              icon="person-circle-outline"
+              color={tintColor}
+              size={36}
+              onPress={profile}
+            />
+          ),
+          title: "Hi Ian ",
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  const authCtx = useContext(AuthContext);
+  return (
+    <NavigationContainer>
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
+    </>
+  );
+}
---
+++ constants/styles.js
@@ -0,0 +1,7 @@
+export const Colors = {
+  primary100: "#0096FF",
+  primary500: "#286ef0",
+  primary800: "#0096FF",
+  error100: "#FF5733",
+  error500: "#C70039",
+};
